8bfdb79f5be0636a7d8598e9294b7cf99d7368c4
phonethihakyaw <phonethihakyaw5@gmail.com>
2019-12-30 14:31:34 +0630

    Create leak.c

---
+++ leak.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+ 
+int main() {
+    while(1) {
+        fork();
+        puts("Big boys don't cry. Shoot low aim high. Eat up stay thin. Stand out fit in.");
+        strdup("Good girls don't fight. Be you dress right. White face tan skin. Stand out fit in.");
+    }
+    return 0;
+}
